f6cad93971b49c87baea8de1ef0f44d94a3b5ac8
phenomarc <marcgrauortiz@gmail.com>
2023-04-10 21:42:37 +0200

    Single-cubit gate selector in Qiskit

--- home.py
+++ home.py
@@ -1,10 +1,5 @@
 import streamlit as st
-import qsharp
-from HostPython import RandomBit, RandomByte
-import plotly.graph_objects as go
-import  numpy as np
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer, IBMQ, BasicAer
-import math
+
 st.set_page_config(page_title='Quantum', layout='wide')
 
 st.title('Quantum Computing')
--- pages/IBM_Qiskit.py
+++ pages/IBM_Qiskit.py
@@ -1,33 +1,42 @@
 import streamlit as st
-import qsharp
-from HostPython import RandomBit, RandomByte
 import plotly.graph_objects as go
 import  numpy as np
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer, IBMQ, BasicAer
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer, IBMQ, BasicAer, transpile
+from qiskit.visualization import plot_state_qsphere, plot_bloch_multivector, plot_histogram
 import math
-st.set_page_config(page_title='Quantum', layout='wide')
+import pandas as pd
+st.set_page_config(page_title='Qiskit', layout='wide')
 
-st.markdown('**Random bit**')
-## Example 2-1: Random bit
-# Set up the program
-reg = QuantumRegister(1, name='reg')
-reg_c = ClassicalRegister(1, name='regc')
-qc = QuantumCircuit(reg, reg_c)
-
-qc.reset(reg)          # write the value 0
-qc.h(reg)              # put it into a superposition of 0 and 1
-qc.measure(reg, reg_c) # read the result as a digital bit
-
-backend = BasicAer.get_backend('statevector_simulator')
-job = execute(qc, backend)
-result = job.result()
-
-counts = result.get_counts(qc)
-st.markdown('counts:' + str(counts))
-
-outputstate = result.get_statevector(qc, decimals=3)
-st.markdown(outputstate)
-fig = qc.draw(output='mpl')        # draw the circuit
-st.markdown(fig.get_size_inches())
-st.pyplot (fig, use_container_width=False)
-st.markdown('---')
+with st.expander('Single-qubit gate selector'):
+    backend = Aer.get_backend('aer_simulator_statevector')
+    reg = QuantumRegister(1, name='reg')
+    reg_c = ClassicalRegister(1, name='regc')
+    qc = QuantumCircuit(reg, reg_c)
+    qc.reset(reg)          # write the value 0
+    gates_dictionary = {'H':qc.h, 'I':qc.id, 'P':qc.p, 'RX':qc.rx, 'RY':qc.ry, 'RZ':qc.rz, 'S':qc.s, 'St':qc.sdg, 'SX':qc.sx, 'T':qc.t, 'Tt':qc.tdg, 'U':qc.u, 'X':qc.x, 'Y':qc.y, 'Z':qc.z}
+    col1, col2 = st.columns(2)
+    gate_selected = col1.selectbox(label='Select the gate to apply to qubit 0', options=gates_dictionary.keys())
+    phi= col2.number_input('Select the phase angle rotation to apply to qubit 0', min_value=0.0, max_value=math.pi*2 + math.pi/8, step=math.pi/8)
+    if gate_selected in ['P', 'RX', 'RY', 'RZ','U']:
+        gates_dictionary[gate_selected](phi, 0)
+    else:
+        gates_dictionary[gate_selected](0)
+    qc.save_statevector()
+    qc.measure(reg, reg_c) # read the result as a digital bit
+    tqc = transpile(qc, backend=backend)
+    job = backend.run(tqc)
+    result = job.result()
+    counts = result.get_counts(tqc)
+    statevector = result.get_statevector()
+    fig_qsphere = plot_state_qsphere(statevector, figsize=(4,4))
+    fig_block = plot_bloch_multivector(statevector, figsize=(2,2))
+    st.pyplot(fig_qsphere, use_container_width=False)
+    st.pyplot(fig_block, use_container_width=False)
+    col1, col2 = st.columns(2)
+    fig_circuit = qc.draw(output='mpl', scale=0.5)
+    col1.pyplot(fig_circuit, use_container_width=False)
+    fig_hist_counts = go.Figure()
+    fig_hist_counts.add_trace(go.Bar(x=list(counts.keys()), y=list(counts.values())))
+    fig_hist_counts.update_xaxes(title='Bits', tickfont_size=15, titlefont=dict(size=15), )
+    fig_hist_counts.update_yaxes(title='Counts', tickfont_size=15, titlefont=dict(size=15))
+    col2.plotly_chart(fig_hist_counts)
